7efa3a88eeb021851be6c39bbeb78204f5d10e59
haager-ca <caius.haager@icloud.com>
2021-06-29 20:29:02 +0200

    added two-way-bindlings

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public cdTitle: string = "Die drei ???1";
+  public showAlert: boolean = true;
+  public newCdTitle: string = "";
+  public cds: string[] = [
+    "Die drei ??? Teil 1",
+    "Die drei ??? Teil 2",
+    "Die drei ??? Teil 3",
+    "Die drei ??? Teil 4",
+    "Die drei ??? Teil 5",
+    "Die drei ??? Teil 6"
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public transformCdTitle(title: string): string {
+    return "Buch: " + title;
+  }
+
+  public addNewCD() {
+    // this.showAlert = !this.showAlert;
+    this.cds.push(this.newCdTitle);
+    this.newCdTitle = "";
+  }
+
+
 }
+
+
